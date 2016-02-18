40e6aed3067594f4d2d8fd6cf86ab39375896892
sweaver2494 <sweaver2494@gmail.com>
2016-02-18 16:22:04 -0600

    Added RMS, Removed Sum

--- Codes/KnnCompRoad.java
+++ Codes/KnnCompRoad.java
@@ -31,7 +31,7 @@ public class KnnCompRoad {
     private static int REDUCED_LENGTH = 8;
     
     //Enter path of feature file if using existing file. Otherwise, a new one is created using files in RawData folder.
-    private static String FEATUREFILEPATH = "Data/FeatureFiles/features_9.csv";
+    private static String FEATUREFILEPATH = "Data/FeatureFiles/features_0.csv";
     
     /*******Must include. This is the file you want to test against feature files*******/
     private static String TESTFILEPATH = "Data/TestData/testData.csv";
@@ -53,12 +53,12 @@ public class KnnCompRoad {
 
 	    	//values contains the values of each sensor type
 	        HashMap<String, ArrayList<Double>> values = new HashMap<String, ArrayList<Double>>();
-	    	//sum is the sum of all values of each sensor type
-	        HashMap<String, Double> sum = new HashMap<String, Double>();
+	        //rms is the root-means-squared of all values of each sensor type
+	        HashMap<String, Double> rms = new HashMap<String, Double>();
 	        //avg is the average of all values of each sensor type
 	        HashMap<String, Double> avg = new HashMap<String, Double>();
-	        //var is the variance of all variances of each sensor type
-	        HashMap<String, Double> var = new HashMap<String, Double>();
+	        //sdv is the standard deviation of all values of each sensor type
+	        HashMap<String, Double> sdv = new HashMap<String, Double>();
 	        
 	        
 	    	String rawDataFolderPath = "Data/RawData/";
@@ -75,25 +75,25 @@ public class KnnCompRoad {
 		    			//read sensor data file and list all values for each feature
 		    	        String classification = readRawData(rawDataFile, values);
 		    	        
-		    	        //calculate the sum, average, and variance of all values for each feature
-		    	        calculateSumAvgVar(values, sum, avg, var);
+		    	        //calculate the average, root-means-squared, and standard deviation of all values for each feature
+		    	        calculateStatistics(values, avg, rms, sdv);
 		    	        
 		    	        //write feature column headers only once
 		    	        if (row == 0) {
-		    	        	writeFeatureFileHeaders(featureFileBuffer, sum, avg, var);
+		    	        	writeFeatureFileHeaders(featureFileBuffer, avg, rms, sdv);
 		    	        }
 		    	        
 		    	        //write single row of feature data, one column per feature
-		    	        writeFeatureFile(featureFileBuffer, sum, avg, var, classification);
+		    	        writeFeatureFile(featureFileBuffer, avg, rms, sdv, classification);
 		    	        
 		    	        //set all default values for each key to reuse containers with the same key ordering
 		    	        for (String key : values.keySet()) {
 		    	        	ArrayList<Double> tempArrayList = values.get(key);
 		    	        	tempArrayList.clear();
 		    	        	
-		    	        	sum.replace(key, (double)0);
 		    	        	avg.replace(key, (double)0);
-		    	        	var.replace(key, (double)0);
+		    	        	rms.replace(key, (double)0);
+		    	        	sdv.replace(key, (double)0);
 		    	        }
 		    	        
 		    	        row++;
@@ -154,52 +154,68 @@ public class KnnCompRoad {
     }
     
     
-    public static void calculateSumAvgVar(HashMap<String, ArrayList<Double>> values, HashMap<String, Double> sum, HashMap<String, Double> avg, HashMap<String, Double> var) {
+    public static void calculateStatistics(HashMap<String, ArrayList<Double>> values, HashMap<String, Double> avg, HashMap<String, Double> rms, HashMap<String, Double> sdv) {
         for (String key : values.keySet()) {
-        	//calculate average and sum for each reading (value) for each sensor type (key)
+        	//calculate average for each reading (value) for each sensor type (key)
         	for (double val : values.get(key)) {
-        		if (sum.containsKey(key)) {
-                	sum.put(key, sum.get(key) + val);
+        		if (avg.containsKey(key)) {
+                	avg.put(key, avg.get(key) + val);
                 } else {
-                	sum.put(key, val);
+                	avg.put(key, val);
                 }
         	}
-        	double keyAvg = sum.get(key) / values.get(key).size();
+        	double keyAvg = avg.get(key) / values.get(key).size();
         	avg.put(key, keyAvg);
         	
-        	//calculate variance for each reading (value) for each sensor type (key)
-        	//variance is the average of the squared differences from the mean
+        	//calculate root-means-squared for each reading (value) for each sensor type (key)
+        	//root-means-squared is square root of the average of squared values
         	for (double val : values.get(key)) {
-        		if (var.containsKey(key)) {
-        			var.put(key, var.get(key) + Math.pow(val - keyAvg, 2));
+        		if (rms.containsKey(key)) {
+        			rms.put(key, rms.get(key) + Math.pow(val, 2));
         		} else {
-                   	var.put(key, Math.pow(val - keyAvg, 2));
+                   	rms.put(key, Math.pow(keyAvg, 2));
                 }
         	}
-        	double keyVar = var.get(key) / values.get(key).size();
-        	var.put(key, keyVar);
+        	double keyRms = rms.get(key) / values.get(key).size();
+        	keyRms = Math.sqrt(keyRms);
+        	rms.put(key, keyRms);
+        	
+        	//calculate standard deviation for each reading (value) for each sensor type (key)
+        	//standard deviation is the square root of the average of the squared differences from the mean
+        	for (double val : values.get(key)) {
+        		if (sdv.containsKey(key)) {
+        			sdv.put(key, sdv.get(key) + Math.pow(val - keyAvg, 2));
+        		} else {
+                   	sdv.put(key, Math.pow(val - keyAvg, 2));
+                }
+        	}
+        	double keySdv = sdv.get(key) / values.get(key).size();
+        	keySdv = Math.sqrt(keySdv);
+        	sdv.put(key, keySdv);
         }
     }
      
     //assumes values exist for sum, avg, and var
-    public static void writeFeatureFileHeaders(BufferedWriter featureFileBuffer, HashMap<String, Double> sum, HashMap<String, Double> avg, HashMap<String, Double> var) {
+    public static void writeFeatureFileHeaders(BufferedWriter featureFileBuffer, HashMap<String, Double> avg, HashMap<String, Double> rms, HashMap<String, Double> sdv) {
 		try {
 			featureFileBuffer.write("classification,");
 			
 	    	//Write column headers.
-			for (String key : sum.keySet()) {
-				featureFileBuffer.write(key + "_sum,");
-			}
 			for (String key : avg.keySet()) {
 				featureFileBuffer.write(key + "_avg,");
 			}
+			
+			for (String key : rms.keySet()) {
+				featureFileBuffer.write(key + "_rms,");
+			}
+			
 			int i = 0;
-			for (String key : var.keySet()) {
+			for (String key : sdv.keySet()) {
 				i++;
-				featureFileBuffer.write(key + "_var");
+				featureFileBuffer.write(key + "_sdv");
 				
 				//don't write comma after last feature
-				if (i < var.keySet().size()) {
+				if (i < sdv.keySet().size()) {
 					featureFileBuffer.write(",");
 				}
 			}
@@ -210,30 +226,30 @@ public class KnnCompRoad {
 		}
     }
     
-    public static void writeFeatureFile(BufferedWriter featureFileBuffer, HashMap<String, Double> sum, HashMap<String, Double> avg, HashMap<String, Double> var, String classification) {
+    public static void writeFeatureFile(BufferedWriter featureFileBuffer, HashMap<String, Double> avg, HashMap<String, Double> rms, HashMap<String, Double> sdv, String classification) {
     	try {
     		featureFileBuffer.write(classification + ",");
     		
 	    	//Write all values for single row.
-			for (String key : sum.keySet()) {
-				double val = sum.get(key);
-				featureFileBuffer.write(Double.toString(val) + ",");
-				System.out.println("Sum: " + Double.toString(val));
-			}
 			for (String key : avg.keySet()) {
 				double val = avg.get(key);
 				featureFileBuffer.write(Double.toString(val) + ",");
 				System.out.println("Avg: " + Double.toString(val));
 			}
+			for (String key : rms.keySet()) {
+				double val = rms.get(key);
+				featureFileBuffer.write(Double.toString(val) + ",");
+				System.out.println("Rms: " + Double.toString(val));
+			}
 			int i = 0;
-			for (String key : var.keySet()) {
+			for (String key : sdv.keySet()) {
 				i++;
-				double val = var.get(key);
+				double val = sdv.get(key);
 				featureFileBuffer.write(Double.toString(val));
-				System.out.println("Var: " + Double.toString(val));
+				System.out.println("Sdv: " + Double.toString(val));
 				
 				//don't write comma after last feature
-				if (i < var.keySet().size()) {
+				if (i < sdv.keySet().size()) {
 					featureFileBuffer.write(",");
 				}
 			}
@@ -300,13 +316,6 @@ public class KnnCompRoad {
     }
     
     public static void testPCA(ArrayList<double[]> fullData, ArrayList<String> fullDataClassification, double[] dataAvg, int dataSize) {
-        /*Random rn = new Random();
-        for (int i = 0; i < 5; i++) {
-            TEST_INDEX = rn.nextInt(15);
-            
-            System.out.println("Test instance is " + TEST_INDEX);
-         */
-    	
     	for (int i = dataSize; i > 0; i--) {
 	    	System.out.println("---------------------------------------------");
 	    	System.out.println("Number of Components = " + Integer.toString(i));
